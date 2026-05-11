b299278895008e15a2265979989c707c7e29bb6a
Lucas-drc <lucaseduardomachado17@gmail.com>
2026-05-11 19:43:17 -0300

    Atualização

--- atividadeFor_1.py
+++ atividadeFor_1.py
@@ -1,17 +1,25 @@
-frutas = ["maçã","banana","laranja","uva","tangerina"]
+# Salva na memória uma variável nome frutas, inserindo uma lista de frutas
+frutas = ["manga", "banana", "abacaxi", "laranja", "uva"]
 
-fruta_favorita = input ("Qual sua fruta favorita?: ")
-
+# Entrada do usuário pedindo para digitar nome da fruta favorita
+fruta_favorita = input("Qual sua fruta favorita?: ")
+# SE a fruta favorita NÃO ESTÁ NA lista frutas
 if fruta_favorita not in frutas:
+    # Faça isso (exibir mensagem e adiciona na lista):
     print("Sua fruta favorita não está na lista!")
-    exit()
+    print("Adicionando...")
+    frutas.append(fruta_favorita)
 
-#Para cada posição (índice) e fruta na lista numerada de frutas
+
+# PARA cada posição (índice) e fruta NA lista numerada de frutas
 for posicao, fruta in enumerate(frutas):
+    # Faça isso:
+    # SE a fruta dessa iteração é igual a fruta favorita
     if fruta == fruta_favorita:
+        # Salva numa nova variável a posição dessa iteração
         posicao_fruta_favorita = posicao
-        break
+        # Quebra o for (faz ele parar)
+        break 
 
-print (f"Minha fruta favorita está na posição índice {posicao_fruta_favorita}")
-
-
+# Saída do algoritmo (print)
+print(f"Minha fruta favorita está na posição índice {posicao_fruta_favorita}")
