c979d2bd560731e3110ceceb4625541fa7ac2d94
Lucas-drc <lucaseduardomachado17@gmail.com>
2026-05-14 21:52:46 -0300

    Atividade While 3

--- atividadeWhile_3.py
+++ atividadeWhile_3.py
@@ -1,12 +1,51 @@
-import random
-numero_secreto = random.randint(0,999999999)
-palpite = 0 #precisou colocar uma variável 0 para inicializar o while
+
+"""
+# MINHA RESOLUÇÃO:
+palpite = []
 tentativas = 0
 
-while palpite != numero_secreto:
+while palpite != 0:
     palpite = int(input("Digite um número: "))
-       
-    tentativas = tentativas + 1
+    if palpite == 0:
+     tentativas = tentativas 
+    else:
+       tentativas = tentativas + 1
+     
+
+if tentativas == 0:
+    print(f"Você digitou o número 0 de primeira!.")
+else:
+    print(f"Você digitou {tentativas} vezes até digitar o número 0.")
+"""
 
 
-print(f"Você digitou {tentativas} números.")
+
+# RESOLUÇÃO DO PROFESSOR:
+# variável contador recebe 0
+# numero recebe -1
+#
+# enquanto numero for diferente de 0:
+#   pergunte "digite um número" e converta para inteiro
+#   numero recebe o que o usuário digitou
+#   se numero for igual a 0:
+#      contador recebe contadora
+#   senao:
+#       contador recebe contador +1
+#exibe tentativas de contagens, contador
+
+contador = 0
+numero = -1
+numeros = []
+
+while numero != 0:
+    numero = int(input("Digite um número: "))
+    if numero != 0:
+        contador = contador + 1
+    numeros.append(numero)
+
+if contador == 0:
+    print(f"Você digitou o número 0 de primeira!.")
+else:
+    print(f"Você digitou {contador} vezes até digitar o número 0.")
+
+print(f"Os número digitados foram:{numeros}.")
